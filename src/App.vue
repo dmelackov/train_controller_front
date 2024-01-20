910992a17f21a998b53a016462f3dda261d45370
Eg0r0k <egokakill@gmail.com>
2024-01-20 22:59:58 +0300

    UI change.
    Co-authored-by: dmelackov <dmelackov@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,51 @@
 <template>
-
+  <Container>
   
-  <div class="station_list">
-    <div class="schedule_selector">
-      <p>Source</p>
-      <select v-model="source_station">
-        <option disabled value="">Please select one</option>
-        <option :value="item.uuid" v-for="item in stations" :key="item.uuid">{{ item.name }}</option>
-      </select>
-      <p>Destination</p>
-      <select v-model="destionation_station">
-        <option disabled value="">Please select one</option>
-        <option :value="item.uuid" v-for="item in stations" :key="item.uuid">{{ item.name }}</option>
-      </select>
-      <label for="is_twoway">Is Twoway</label>
-      <input v-model="is_twoway" type="checkbox" name="Twoway" id="is_twoway">
-      <button @click="send">Send</button>
+
+    <div class="cards__container">
+      <StationCardv2 v-for="station in sorted_stations" :key="station.uuid" :station_uuid="station.uuid" />
     </div>
-    <hr>
-    <template v-for="station in stations" v-bind:key="station.uuid">
-      <StationCard :station_id="station.uuid"></StationCard>
-    </template>
-  </div>
+
+  </Container>
 </template>
 
 <script setup>
-import StationCard from './components/StationCard.vue';
-import {api, websocket_api} from './api';
-import { onMounted, ref } from 'vue';
 
-const stations = ref([])
+import StationCardv2 from './components/StationCardv2.vue';
+import Container from './components/Container.vue';
+import { useStationStore } from './store/stationStore'
+import { onMounted, computed } from 'vue';
+import { websocket_api } from './api';
 
-const source_station = ref("")
-const destionation_station = ref("")
-const is_twoway = ref(false)
+const store = useStationStore()
 
-function send() {
-  if(source_station.value && destionation_station.value){
-    api.set_station_schedule(source_station.value, destionation_station.value, is_twoway.value)
-  }
-}
+const sorted_stations = computed(() => {
+  return  store.stations.slice().sort((a, b) => {
+        if(!a.info || !b.info) return 0
+        const hasTrainA = !!a.info.train_name || '';
+        const hasTrainB = !!b.info.train_name || '';
+        if (hasTrainA && !hasTrainB)
+          return -1
+        if (!hasTrainA && hasTrainB)
+          return 1
+        return 0
+    });
+});
+
 
 onMounted(async () => {
-  websocket_api.emitter.on("update_station_list", async () => {
-    stations.value = await api.get_stations()
-  })
-  websocket_api.open()
-  stations.value = await api.get_stations()
-})
+  websocket_api.open();
+  await store.getStations()
+});
+
 
 
 </script>
 
 <style lang="scss">
-.schedule_selector{
-  display: flex;
-  align-items: center;
-  gap: 10px;
+.cards__container {
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  gap: 20px;
 }
 </style>
---
+++ src/store/stationStore.js
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+import { api, websocket_api } from "../api";
+import {ref} from 'vue'
+export const useStationStore = defineStore('station',()=>
+{
+    const stations = ref([])
+
+    websocket_api.emitter.on("update_station_list", getStations);
+    websocket_api.emitter.on("update_station", (event) => {
+        updateStationInfo(event.uuid)
+    })
+
+    async function getStations(){
+        try {
+            stations.value = await api.get_stations();
+            
+           await Promise.all(stations.value.map(async (station) => {
+                if(station.info)
+                    return;
+                station.info = await fetchStationInfo(station.uuid)
+            }))
+          } catch (error) {
+            console.error("Ошибка при инициализации компонента:", error);
+          }
+    }
+    async function updateStationInfo(uuid){
+        stations.value.find((st) => st.uuid == uuid).info = await fetchStationInfo(uuid)
+    }
+
+    async function fetchStationInfo(uuid)
+    {
+        try{
+            return await api.get_station_info(uuid);
+        }
+        catch(error)
+        {
+            console.error('Ошибка фетча информации', error)
+            throw error
+        }
+    }
+    return  {getStations,stations,fetchStationInfo,updateStationInfo}
+}
+)
